c2fd0566cabb6a9ebf1ccf49da3d358a635c9da3
MHO <mauricio.hdez.o.10@gmail.com>
2020-07-18 15:23:53 -0500

    Initializing domain module with entity Character and related.

---
+++ domain/src/main/java/com/platzi/android/rickandmorty/domain/Entities.kt
@@ -0,0 +1,23 @@
+package com.platzi.android.rickandmorty.domain
+
+data class Character(
+    val id: Int,
+    val name: String,
+    val image: String?,
+    val gender: String,
+    val species: String,
+    val status: String,
+    val origin: Origin,
+    val location: Location,
+    val episodeList: List<String>
+)
+
+data class Location(
+    val name: String,
+    val url: String
+)
+
+data class Origin(
+    val name: String,
+    val url: String
+)
